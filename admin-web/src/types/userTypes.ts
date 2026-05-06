941ae77c13dd759a278c5715309db4e5f52b1632
JoseNarvaezMtz <jose.narvaez.mtz05@gmail.com>
2026-05-05 19:52:22 -0600

    Interfaces para manejo de CRUD de usuario

---
+++ admin-web/src/types/userTypes.ts
@@ -0,0 +1,21 @@
+export interface BackendUsuarioCol {
+  colonia: {
+    idColonia: number;
+    nombre: string;
+  };
+}
+export interface BackendUser {
+  idUsuario: number;
+  nombre: string;
+  email: string;
+  admin: boolean;
+  createdAt: string;
+  usuariosCols: BackendUsuarioCol[];
+}
+export interface UpdateUserResponse {
+  mensaje: string;
+  modificado: boolean;
+}
+export interface DeleteUserResponse {
+  mensaje: string;
+}
