4aec6e48bf9f08869daa737bc8bc19605dc19dd1
HarimDuenas <harimenriqueduenas@outlook.com>
2026-05-06 09:03:16 -0600

    Se cambio el tipo de import para que fuera tipo 'type'

--- admin-web/src/services/avistamientosApi.ts
+++ admin-web/src/services/avistamientosApi.ts
@@ -1,4 +1,4 @@
-import { BackendAvistamiento } from "../types/models";
+import type { BackendAvistamiento } from "../types/models";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
