23073e6d390350af924b4bf0f777d0217b24855a
Santos <emiliano.santos_@outlook.com>
2026-05-28 18:00:14 -0600

    Se cambió la ruta para que todo el proyecto funcione bajo /admin.

--- admin-web/src/main.tsx
+++ admin-web/src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
       <AlertProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/admin">
           <App />
         </BrowserRouter>
       </AlertProvider>
