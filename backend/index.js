725442aece3fa49e3f5f140d58d9b94a1f3026d4
Santos <emiliano.santos_@outlook.com>
2026-05-29 20:23:32 -0600

    FIX/Backend: Se hizo dinámico el puerto de ejecución del backend.

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors'); 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // IMPORT DE RUTAS
 const userRoutes = require('./routes/user.routes');
