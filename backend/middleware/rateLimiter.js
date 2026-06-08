4ba37afd400986bf350e83417ce6538864eb59ef
HarimDuenas <harimenriqueduenas@outlook.com>
2026-06-08 02:23:19 -0600

    Cambio de comentario

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 const authLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutos
   max: 5, // Límite de 5 peticiones
-  message: { mensaje: "Demasiados intentos de inicio de sesión desde esta IP, por favor inténtalo de nuevo después de 5 minutos" },
+  message: { mensaje: "Demasiados intentos de inicio de sesión, por favor inténtalo de nuevo después de 5 minutos" },
   standardHeaders: true,
   legacyHeaders: false,
 });
