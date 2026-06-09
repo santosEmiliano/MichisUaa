4e371efdca9c03db35e14caa8bf741f08e1e02d8
santosEmiliano <emiliano.santos_@outlook.com>
2026-06-09 15:08:30 -0600

    Se agregaron las dos dependencias para crear los validators y el santizador. Aparte, se incluyó una de estas dependencias en el servidor del backend para sanitizar y prevenir peticiones a la api con ataques de tipo XSS.

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const hpp = require('hpp');
 const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 3000;
+const xssClean = require('xss-clean');
 
 // IMPORT DE RUTAS
 const userRoutes = require('./routes/user.routes');
@@ -38,6 +39,7 @@ app.use(cors({
 app.use(morgan('combined')); // Logging de peticiones
 app.use(express.json({ limit: '10kb' })); // Límite tamaño JSON
 app.use(express.urlencoded({ extended: true, limit: '10kb' })); // Límite tamaño URL-encoded
+app.use(xssClean()); // Prevención de Cross-Site Scripting (XSS)
 app.use(hpp()); // Prevención de contaminación de parámetros
 app.set('trust proxy', 1);
 
